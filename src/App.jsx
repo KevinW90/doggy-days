a412adc6474ee9585eeaa1a1b211d4575ee6617d
Kevin Williams <kevinjwilliams76@gmail.com>
2021-05-29 09:41:19 -0400

    theme applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/styles';
 import Header from './components/Header'
 import MenuIcon from '@material-ui/icons/Menu';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-
+import { theme } from './themes/theme'
 
 function App() {
   return (
-    <Router>
-      <Header
-        left={{
-          icon: MenuIcon
-        }}
-        center={{
-          text: 'My Pets'
-        }}
-        right={{
-          icon: AddCircleIcon
-        }}
-      />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header
+          left={{
+            icon: MenuIcon
+          }}
+          center={{
+            text: 'My Pets'
+          }}
+          right={{
+            icon: AddCircleIcon
+          }}
+        />
     </Router>
+    </ThemeProvider>
+    
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
 import SvgIcon from "@material-ui/core/SvgIcon";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles} from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles(theme => ({
+  header: {
+    background: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '25px 20px',
+    fontSize: '1.25rem'
+  },
+  icon: {
+    fontSize: theme.typography.iconSize
+  }
+}))
 
 function Header({ left, center, right }) {
+  const classes = useStyles()
+
   return (
-    <section className="header">
-      <div className="header_left">
-        <SvgIcon component={left.icon} />
-      </div>
-
-      <div className="header_center">
-        <span>{center.text}</span>
-      </div>
-
-      <div className="header_right">
-        <SvgIcon component={right.icon} />
-      </div>
+    <section className={classes.header}>
+      <SvgIcon component={left.icon} className={classes.icon} />
+      <Typography variant="h5">{center.text}</Typography>
+      <SvgIcon component={right.icon} className={classes.icon} />
     </section>    
   )
 }
